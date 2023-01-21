eb7658b82f1e7067736bbf133e591bce16d653b1
shravan <shravannkumarbavandla@gmail.com>
2023-01-21 15:02:02 +0000

    project

---
+++ exportMultipleValues.mjs
@@ -0,0 +1,8 @@
+const myArray = ["camel", 265, true, "5.6"];
+const bulb = { watts: 10, type: "LED" };
+
+function multiplyByFour(a, b) {
+  return a * b;
+}
+
+export { myArray, bulb, multiplyByFour };
